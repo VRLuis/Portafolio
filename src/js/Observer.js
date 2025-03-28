ab80ff2b35c81f120ace589e87a8c79ed9ca4d0e
VRLuis <luisvillarroldan1010@gmail.com>
2025-03-28 14:18:35 +0100

    Add intersection observers for new animation effects and update styles for hidden elements

--- src/js/Observer.js
+++ src/js/Observer.js
@@ -1,4 +1,4 @@
-const observer = new  IntersectionObserver((entries) => {
+const observerHidden = new  IntersectionObserver((entries) => {
     entries.forEach((entry)=>{
         if(entry.isIntersecting){
             entry.target.classList.add('show');
@@ -8,5 +8,26 @@ const observer = new  IntersectionObserver((entries) => {
     })
 })
 
+const observerTiny = new  IntersectionObserver((entries) => {
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add('tiny', 'show');
+        } 
+    })
+})
+
+const observerSlide = new  IntersectionObserver((entries) => {
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add('slideInRight');
+        }
+    })
+})
+
 const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el))
+const tinyElements = document.querySelectorAll('.tiny');
+const slideElements = document.querySelectorAll('.slide');
+
+hiddenElements.forEach((el) => observerHidden.observe(el))
+slideElements.forEach((el) => observerSlide.observe(el))
+tinyElements.forEach((el) => observerTiny.observe(el))
